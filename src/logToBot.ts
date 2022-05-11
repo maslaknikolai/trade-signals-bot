54a28468c6df4f1d9cee8439ddfa26a90144c498
Nikolay Maslak <nikolay.maslak@onlinesup.com>
2022-05-11 18:53:45 +0300

    alive bot cmd

--- src/logToBot.ts
+++ src/logToBot.ts
@@ -2,6 +2,8 @@ import { Telegraf } from 'telegraf'
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+bot.command('check', (ctx) => ctx.reply('Жив'))
+
 export default function logToBot(msg: string) {
     bot.telegram.sendMessage(process.env.BOT_LOG_CHAT_ID, msg);
 }
