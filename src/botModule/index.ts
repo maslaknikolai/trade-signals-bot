5691e40007814a69d29780c3edc2bd71e743366e
Nikolay Maslak <nikolay.maslak@onlinesup.com>
2022-05-14 19:08:20 +0300

    mistype

--- src/botModule/index.ts
+++ src/botModule/index.ts
@@ -7,7 +7,7 @@ import logToBot from './logToBot'
 const bot = new Telegraf(BOT_TOKEN)
 
 bot.command('start', (ctx) => {
-    ctx.reply(`This bot allows you to automatically sell on copts.com. If you want to use it please contact ${TELEGRAM_OWNER_LOGIN}`);
+    ctx.reply(`This bot allows you to automatically sell on cotps.com. If you want to use it please contact ${TELEGRAM_OWNER_LOGIN}`);
     logToBot(TELEGRAM_OWNER_CHAT_ID, `Start command recevied.\n${JSON.stringify(ctx.update.message.from, null, 2)}`)
 })
 
