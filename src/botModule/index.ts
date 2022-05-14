60da704bcb65cdd968739c3890815d49b13bf0ce
Nikolay Maslak <nikolay.maslak@onlinesup.com>
2022-05-14 18:50:20 +0300

    start command

--- src/botModule/index.ts
+++ src/botModule/index.ts
@@ -1,9 +1,15 @@
 import getCredentialByChatId from '../credentialsModule/getCredentialByChatId'
 import setIsCredentialEnabled from '../credentialsModule/setIsCredentialEnabled'
 import { Telegraf } from 'telegraf'
-import { BOT_TOKEN } from '../config'
+import { BOT_TOKEN, TELEGRAM_OWNER_CHAT_ID, TELEGRAM_OWNER_LOGIN } from '../config'
+import logToBot from './logToBot'
 
 const bot = new Telegraf(BOT_TOKEN)
+
+bot.command('start', (ctx) => {
+    ctx.reply(`This bot allows you to automatically sell on copts.com. If you want to use it please contact ${TELEGRAM_OWNER_LOGIN}`);
+    logToBot(TELEGRAM_OWNER_CHAT_ID, `Start command recevied.\n${JSON.stringify(ctx.update.message.from, null, 2)}`)
+})
 
 bot.command('check', (ctx) => ctx.reply('🌚'))
 
