a9044962038df3f507ffb8952fdcf50d64c3774f
Nikolai Maslak <maslaknikolai@ya.ru>
2022-08-11 19:38:26 +0300

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import 'dotenv/config';
 import startServer from './startServer';
 import connect from './db'
 import startBot from './botModule/startBot';
-import messageToOrderRequestData from './messageToOrderRequestData';
-import createOrder from './createOrder';
 
 async function main() {
     startServer()
--- src/startServer/getKline/index.ts
+++ src/startServer/getKline/index.ts
@@ -4,14 +4,14 @@ import createBybitClient from '../../createBybitClient';
 export default async function getKline(coin: string) {
     const client = createBybitClient()
 
-    const tickersResult = await client.getKline({
+    const klineResponse = await client.getKline({
         symbol: `${coin}USDT`,
         interval: '30',
         from: Math.round((Date.now().valueOf() - (1000 * 60 * 60 * 24 * 28 * 6)) / 1000),
         limit: 200,
     })
 
-    // fs.writeFileSync('kline.json', JSON.stringify(tickersResult.result, null, 2))
+    // fs.writeFileSync('kline.json', JSON.stringify(klineResponse.result, null, 2))
 
-    return tickersResult.result
+    return klineResponse.result
 }
